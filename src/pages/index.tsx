b576464b4cc5159e4fb9fce39c40d0098bdfecd6
Hector Novoa <hectornovoadita2000@gmail.com>
2023-11-19 09:50:31 -0500

    feat: added fav icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,12 +16,7 @@ export const appRoutes = [
       path: '/',
       element: <RootLayout/>,
       children: [
-         { index: true, element: <Navigate to='/home' replace/>},
          { index: true, element: <Navigate to='/map' replace/>},
-         {
-            path: 'home',
-            element: <Home/>
-         },
          {
             path: 'map',
             element: <Map/>
