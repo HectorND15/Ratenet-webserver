8a420d9add9a57c03635a0b47e0965f6f0f1535d
Hector Novoa <hectornovoadita2000@gmail.com>
2023-11-22 04:01:41 -0500

    heatmap

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -6,25 +6,40 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-function createCircleMarker(lat, lng, value) {
-   var color;
-   if (value <= 1) {
-      color = 'red';
-   } else if (value <= 3) {
-      color = 'yellow';
+
+
+function getColor(value) {
+   var colors = {
+         2: 'red',
+         3: 'yellow',
+         5: 'green'
+   };
+
+   if (value <= 2) return colors[2];
+   if (value >= 5) return colors[5];
+
+   var r, g, b = 0;
+   if (value < 3) {
+         // Interpolar entre rojo y amarillo
+         r = 255;
+         g = Math.floor(255 * (value - 2));
    } else {
-      color = 'green';
+         // Interpolar entre amarillo y verde
+         r = Math.floor(255 * (1 - (value - 3) / 2));
+         g = 255;
    }
 
+   return `rgb(${r},${g},${b})`;
+}
+
+function createCircleMarker(lat, lng, value) {
    return L.circleMarker([lat, lng], {
-      radius: 8,
-      fillColor: color,
-      color: '#000',
-      weight: 1,
-      opacity: 1,
-      fillOpacity: 0.8
-      }).addTo(map);
-   }
+         radius: 10, // Tamaño más grande
+         fillColor: getColor(value),
+         fillOpacity: 0.8,
+         color: 'none' // Sin borde negro
+   }).addTo(map);
+}
 
    function fetchData(tableName) {
       fetch(`/get-map-data?table=${tableName}`)
